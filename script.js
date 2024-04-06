fff1381bf390a9e247e0a09fef32494dcb085706
unknown <shodmonovjavohir56@gmail.com>
2024-04-06 14:25:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// let str1 = "Hello World"
+//     str2 = `Hello World`
+
+// // console.log(str1 == str2);
+// let str1 = 'O\'zbekiston'; // two lines using a "newline symbol"
+
+// // two lines using a normal newline and backticks
+// let str2 = `Hello
+// World
+// Ishladiyu`;
+
+// alert(str1); // true
+
+// let str = "Samsung"
+// alert(str[str.length - 1])
+// alert(str.at(-1))
+
+// let str = 'Hello';
+// alert(str[0].toLowerCase() + str)
+
+// let str = "Widget with id"
+
+// alert(str.startsWith('Widget '))
+// alert(str.endsWith('id') + '84')
+
+// let str = "Hello lo lo lo lo lo "
+// alert(str.includes('lo', 7))
+
+// Bu tanlanga elementlarni nusxa olsih uchun ishlatiladi
+// slice()
+// let str = 'stringify'
+// alert(str.slice(0, 3))
+// let str = 'stringify'
+// alert(str.slice(-4))
+// alert(str)
+
+// substring()
+// let str = 'stringify'
+// alert(str.substring(-4, 2))
